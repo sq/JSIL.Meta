57eeef77df08c3b9e643987a8632c592fcdfb13c
Katelyn Gadd <kg@luminance.org>
2015-08-31 13:15:47 -0700

    Add a script to install PCL reference assemblies so you can build Meta reliably on linux

---
+++ install-pcl-reference-assemblies.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# installs the PCL reference assemblies so that xbuild can build jsil.meta
+# http://blog.csmac.nz/travis-ci-c-and-pcl/
+
+# make sure to run this with 'source' i.e. 
+#   source ./install-pci-reference-assemblies.sh
+# or
+#   . ./install-pci-reference-assemblies.sh
+
+SCRIPTPATH=`realpath "$BASH_SOURCE"`
+DIRNAME=`dirname $SCRIPTPATH`
+OUTDIR=$DIRNAME/temp
+
+echo $OUTDIR
+curl -S http://storage.bos.xamarin.com/bot-provisioning/PortableReferenceAssemblies-2014-04-14.zip --output /tmp/pcl-assemblies.zip
+rm -rf $OUTDIR
+mkdir $OUTDIR
+unzip -o -q /tmp/pcl-assemblies.zip -d $OUTDIR/pcl-assemblies && mv $OUTDIR/pcl-assemblies/PortableReferenceAssemblies-2014-04-14 $OUTDIR/pcl-assemblies/.NETPortable
+export XBUILD_FRAMEWORK_FOLDERS_PATH=$OUTDIR/pcl-assemblies
