1bba443d09144a619b12a82bc9751a3db0530c08
ik <igor@kiselev.spb.ru>
2015-12-08 15:54:43 -0800

    JsObject and JsObject.JsFunction is closed hierarchy now.

--- JSObject.cs
+++ JSObject.cs
@@ -4,17 +4,24 @@ namespace JSIL {
 
     using JSIL.Meta;
 
-    public abstract class JsObject
+    public class JsObject
     {
+        private JsObject()
+        {
+        }
+
         //Should work same as Builtins.Global[key]
         public static JsObject Global(string key)
         {
             throw new NotSupportedException("Not available outside JS");
         }
-    }
 
-    public abstract class JsFunction
-    {
+        public sealed class JsFunction : JsObject
+        {
+            private JsFunction() : base()
+            {
+            }
+        }
     }
 
     public static class JsObjectHelpers
@@ -44,163 +51,163 @@ namespace JSIL {
         }
 
         [JSReplacement("$target[$key]()")]
-        public static JsObject Call(this JsFunction target, string key)
+        public static JsObject Call(this JsObject.JsFunction target, string key)
         {
             throw new NotSupportedException("Not available outside JS");
         }
 
         [JSReplacement("$target[$key]($arg1)")]
-        public static JsObject Call<TArg1>(this JsFunction target, string key, TArg1 arg1)
+        public static JsObject Call<TArg1>(this JsObject.JsFunction target, string key, TArg1 arg1)
         {
             throw new NotSupportedException("Not available outside JS");
         }
 
         [JSReplacement("$target[$key]($arg1, $arg2)")]
-        public static JsObject Call<TArg1, TArg2>(this JsFunction target, string key, TArg1 arg1, TArg2 arg2)
+        public static JsObject Call<TArg1, TArg2>(this JsObject.JsFunction target, string key, TArg1 arg1, TArg2 arg2)
         {
             throw new NotSupportedException("Not available outside JS");
         }
 
         [JSReplacement("$target[$key]($arg1, $arg2, $arg3)")]
-        public static JsObject Call<TArg1, TArg2, TArg3>(this JsFunction target, string key, TArg1 arg1, TArg2 arg2, TArg3 arg3)
+        public static JsObject Call<TArg1, TArg2, TArg3>(this JsObject.JsFunction target, string key, TArg1 arg1, TArg2 arg2, TArg3 arg3)
         {
             throw new NotSupportedException("Not available outside JS");
         }
 
         [JSReplacement("$target[$key]($arg1, $arg2, $arg3, $arg4)")]
-        public static JsObject Call<TArg1, TArg2, TArg3, TArg4>(this JsFunction target, string key, TArg1 arg1, TArg2 arg2, TArg3 arg3, TArg4 arg4)
+        public static JsObject Call<TArg1, TArg2, TArg3, TArg4>(this JsObject.JsFunction target, string key, TArg1 arg1, TArg2 arg2, TArg3 arg3, TArg4 arg4)
         {
             throw new NotSupportedException("Not available outside JS");
         }
 
         [JSReplacement("$target[$key]($arg1, $arg2, $arg3, $arg4, $arg5)")]
-        public static JsObject Call<TArg1, TArg2, TArg3, TArg4, TArg5>(this JsFunction target, string key, TArg1 arg1, TArg2 arg2, TArg3 arg3, TArg4 arg4, TArg5 arg5)
+        public static JsObject Call<TArg1, TArg2, TArg3, TArg4, TArg5>(this JsObject.JsFunction target, string key, TArg1 arg1, TArg2 arg2, TArg3 arg3, TArg4 arg4, TArg5 arg5)
         {
             throw new NotSupportedException("Not available outside JS");
         }
 
         [JSReplacement("$target[$key]($arg1, $arg2, $arg3, $arg4, $arg5, $arg6)")]
-        public static JsObject Call<TArg1, TArg2, TArg3, TArg4, TArg5, TArg6>(this JsFunction target, string key, TArg1 arg1, TArg2 arg2, TArg3 arg3, TArg4 arg4, TArg5 arg5, TArg6 arg6)
+        public static JsObject Call<TArg1, TArg2, TArg3, TArg4, TArg5, TArg6>(this JsObject.JsFunction target, string key, TArg1 arg1, TArg2 arg2, TArg3 arg3, TArg4 arg4, TArg5 arg5, TArg6 arg6)
         {
             throw new NotSupportedException("Not available outside JS");
         }
 
         [JSReplacement("$target[$key]($arg1, $arg2, $arg3, $arg4, $arg5, $arg6, $arg7)")]
-        public static JsObject Call<TArg1, TArg2, TArg3, TArg4, TArg5, TArg6, TArg7>(this JsFunction target, string key, TArg1 arg1, TArg2 arg2, TArg3 arg3, TArg4 arg4, TArg5 arg5, TArg6 arg6, TArg7 arg7)
+        public static JsObject Call<TArg1, TArg2, TArg3, TArg4, TArg5, TArg6, TArg7>(this JsObject.JsFunction target, string key, TArg1 arg1, TArg2 arg2, TArg3 arg3, TArg4 arg4, TArg5 arg5, TArg6 arg6, TArg7 arg7)
         {
             throw new NotSupportedException("Not available outside JS");
         }
 
         [JSReplacement("$target[$key]($arg1, $arg2, $arg3, $arg4, $arg5, $arg6, $arg7, $arg8)")]
-        public static JsObject Call<TArg1, TArg2, TArg3, TArg4, TArg5, TArg6, TArg7, TArg8>(this JsFunction target, string key, TArg1 arg1, TArg2 arg2, TArg3 arg3, TArg4 arg4, TArg5 arg5, TArg6 arg6, TArg7 arg7, TArg8 arg8)
+        public static JsObject Call<TArg1, TArg2, TArg3, TArg4, TArg5, TArg6, TArg7, TArg8>(this JsObject.JsFunction target, string key, TArg1 arg1, TArg2 arg2, TArg3 arg3, TArg4 arg4, TArg5 arg5, TArg6 arg6, TArg7 arg7, TArg8 arg8)
         {
             throw new NotSupportedException("Not available outside JS");
         }
 
         [JSReplacement("$target.call(null)")]
-        public static JsObject Invoke(this JsFunction target)
+        public static JsObject Invoke(this JsObject.JsFunction target)
         {
             throw new NotSupportedException("Not available outside JS");
         }
 
         [JSReplacement("$target.call(null, $arg1)")]
-        public static JsObject Invoke<TArg1>(this JsFunction target, TArg1 arg1)
+        public static JsObject Invoke<TArg1>(this JsObject.JsFunction target, TArg1 arg1)
         {
             throw new NotSupportedException("Not available outside JS");
         }
 
         [JSReplacement("$target.call(null, $arg1, $arg2)")]
-        public static JsObject Invoke<TArg1, TArg2>(this JsFunction target, TArg1 arg1, TArg2 arg2)
+        public static JsObject Invoke<TArg1, TArg2>(this JsObject.JsFunction target, TArg1 arg1, TArg2 arg2)
         {
             throw new NotSupportedException("Not available outside JS");
         }
 
         [JSReplacement("$target.call(null, $arg1, $arg2, $arg3)")]
-        public static JsObject Invoke<TArg1, TArg2, TArg3>(this JsFunction target, TArg1 arg1, TArg2 arg2, TArg3 arg3)
+        public static JsObject Invoke<TArg1, TArg2, TArg3>(this JsObject.JsFunction target, TArg1 arg1, TArg2 arg2, TArg3 arg3)
         {
             throw new NotSupportedException("Not available outside JS");
         }
 
         [JSReplacement("$target.call(null, $arg1, $arg2, $arg3, $arg4)")]
-        public static JsObject Invoke<TArg1, TArg2, TArg3, TArg4>(this JsFunction target, TArg1 arg1, TArg2 arg2, TArg3 arg3, TArg4 arg4)
+        public static JsObject Invoke<TArg1, TArg2, TArg3, TArg4>(this JsObject.JsFunction target, TArg1 arg1, TArg2 arg2, TArg3 arg3, TArg4 arg4)
         {
             throw new NotSupportedException("Not available outside JS");
         }
 
         [JSReplacement("$target.call(null, $arg1, $arg2, $arg3, $arg4, $arg5)")]
-        public static JsObject Invoke<TArg1, TArg2, TArg3, TArg4, TArg5>(this JsFunction target, TArg1 arg1, TArg2 arg2, TArg3 arg3, TArg4 arg4, TArg5 arg5)
+        public static JsObject Invoke<TArg1, TArg2, TArg3, TArg4, TArg5>(this JsObject.JsFunction target, TArg1 arg1, TArg2 arg2, TArg3 arg3, TArg4 arg4, TArg5 arg5)
         {
             throw new NotSupportedException("Not available outside JS");
         }
 
         [JSReplacement("$target.call(null, $arg1, $arg2, $arg3, $arg4, $arg5, $arg6)")]
-        public static JsObject Invoke<TArg1, TArg2, TArg3, TArg4, TArg5, TArg6>(this JsFunction target, TArg1 arg1, TArg2 arg2, TArg3 arg3, TArg4 arg4, TArg5 arg5, TArg6 arg6)
+        public static JsObject Invoke<TArg1, TArg2, TArg3, TArg4, TArg5, TArg6>(this JsObject.JsFunction target, TArg1 arg1, TArg2 arg2, TArg3 arg3, TArg4 arg4, TArg5 arg5, TArg6 arg6)
         {
             throw new NotSupportedException("Not available outside JS");
         }
 
         [JSReplacement("$target.call(null, $arg1, $arg2, $arg3, $arg4, $arg5, $arg6, $arg7)")]
-        public static JsObject Invoke<TArg1, TArg2, TArg3, TArg4, TArg5, TArg6, TArg7>(this JsFunction target, TArg1 arg1, TArg2 arg2, TArg3 arg3, TArg4 arg4, TArg5 arg5, TArg6 arg6, TArg7 arg7)
+        public static JsObject Invoke<TArg1, TArg2, TArg3, TArg4, TArg5, TArg6, TArg7>(this JsObject.JsFunction target, TArg1 arg1, TArg2 arg2, TArg3 arg3, TArg4 arg4, TArg5 arg5, TArg6 arg6, TArg7 arg7)
         {
             throw new NotSupportedException("Not available outside JS");
         }
 
         [JSReplacement("$target.call(null, $arg1, $arg2, $arg3, $arg4, $arg5, $arg6, $arg7, $arg8)")]
-        public static JsObject Invoke<TArg1, TArg2, TArg3, TArg4, TArg5, TArg6, TArg7, TArg8>(this JsFunction target, TArg1 arg1, TArg2 arg2, TArg3 arg3, TArg4 arg4, TArg5 arg5, TArg6 arg6, TArg7 arg7, TArg8 arg8)
+        public static JsObject Invoke<TArg1, TArg2, TArg3, TArg4, TArg5, TArg6, TArg7, TArg8>(this JsObject.JsFunction target, TArg1 arg1, TArg2 arg2, TArg3 arg3, TArg4 arg4, TArg5 arg5, TArg6 arg6, TArg7 arg7, TArg8 arg8)
         {
             throw new NotSupportedException("Not available outside JS");
         }
 
         [JSReplacement("new $target()")]
-        public static JsObject New(this JsFunction target)
+        public static JsObject New(this JsObject.JsFunction target)
         {
             throw new NotSupportedException("Not available outside JS");
         }
 
         [JSReplacement("new $target($arg1)")]
-        public static JsObject New<TArg1>(this JsFunction target, TArg1 arg1)
+        public static JsObject New<TArg1>(this JsObject.JsFunction target, TArg1 arg1)
         {
             throw new NotSupportedException("Not available outside JS");
         }
 
         [JSReplacement("new $target($arg1, $arg2)")]
-        public static JsObject New<TArg1, TArg2>(this JsFunction target, TArg1 arg1, TArg2 arg2)
+        public static JsObject New<TArg1, TArg2>(this JsObject.JsFunction target, TArg1 arg1, TArg2 arg2)
         {
             throw new NotSupportedException("Not available outside JS");
         }
 
         [JSReplacement("new $target($arg1, $arg2, $arg3)")]
-        public static JsObject New<TArg1, TArg2, TArg3>(this JsFunction target, TArg1 arg1, TArg2 arg2, TArg3 arg3)
+        public static JsObject New<TArg1, TArg2, TArg3>(this JsObject.JsFunction target, TArg1 arg1, TArg2 arg2, TArg3 arg3)
         {
             throw new NotSupportedException("Not available outside JS");
         }
 
         [JSReplacement("new $target($arg1, $arg2, $arg3, $arg4)")]
-        public static JsObject New<TArg1, TArg2, TArg3, TArg4>(this JsFunction target, TArg1 arg1, TArg2 arg2, TArg3 arg3, TArg4 arg4)
+        public static JsObject New<TArg1, TArg2, TArg3, TArg4>(this JsObject.JsFunction target, TArg1 arg1, TArg2 arg2, TArg3 arg3, TArg4 arg4)
         {
             throw new NotSupportedException("Not available outside JS");
         }
 
         [JSReplacement("new $target($arg1, $arg2, $arg3, $arg4, $arg5)")]
-        public static JsObject New<TArg1, TArg2, TArg3, TArg4, TArg5>(this JsFunction target, TArg1 arg1, TArg2 arg2, TArg3 arg3, TArg4 arg4, TArg5 arg5)
+        public static JsObject New<TArg1, TArg2, TArg3, TArg4, TArg5>(this JsObject.JsFunction target, TArg1 arg1, TArg2 arg2, TArg3 arg3, TArg4 arg4, TArg5 arg5)
         {
             throw new NotSupportedException("Not available outside JS");
         }
 
         [JSReplacement("new $target($arg1, $arg2, $arg3, $arg4, $arg5, $arg6)")]
-        public static JsObject New<TArg1, TArg2, TArg3, TArg4, TArg5, TArg6>(this JsFunction target, TArg1 arg1, TArg2 arg2, TArg3 arg3, TArg4 arg4, TArg5 arg5, TArg6 arg6)
+        public static JsObject New<TArg1, TArg2, TArg3, TArg4, TArg5, TArg6>(this JsObject.JsFunction target, TArg1 arg1, TArg2 arg2, TArg3 arg3, TArg4 arg4, TArg5 arg5, TArg6 arg6)
         {
             throw new NotSupportedException("Not available outside JS");
         }
 
         [JSReplacement("new $target($arg1, $arg2, $arg3, $arg4, $arg5, $arg6, $arg7)")]
-        public static JsObject New<TArg1, TArg2, TArg3, TArg4, TArg5, TArg6, TArg7>(this JsFunction target, TArg1 arg1, TArg2 arg2, TArg3 arg3, TArg4 arg4, TArg5 arg5, TArg6 arg6, TArg7 arg7)
+        public static JsObject New<TArg1, TArg2, TArg3, TArg4, TArg5, TArg6, TArg7>(this JsObject.JsFunction target, TArg1 arg1, TArg2 arg2, TArg3 arg3, TArg4 arg4, TArg5 arg5, TArg6 arg6, TArg7 arg7)
         {
             throw new NotSupportedException("Not available outside JS");
         }
 
         [JSReplacement("new $target($arg1, $arg2, $arg3, $arg4, $arg5, $arg6, $arg7, $arg8)")]
-        public static JsObject New<TArg1, TArg2, TArg3, TArg4, TArg5, TArg6, TArg7, TArg8>(this JsFunction target, TArg1 arg1, TArg2 arg2, TArg3 arg3, TArg4 arg4, TArg5 arg5, TArg6 arg6, TArg7 arg7, TArg8 arg8)
+        public static JsObject New<TArg1, TArg2, TArg3, TArg4, TArg5, TArg6, TArg7, TArg8>(this JsObject.JsFunction target, TArg1 arg1, TArg2 arg2, TArg3 arg3, TArg4 arg4, TArg5 arg5, TArg6 arg6, TArg7 arg7, TArg8 arg8)
         {
             throw new NotSupportedException("Not available outside JS");
         }
